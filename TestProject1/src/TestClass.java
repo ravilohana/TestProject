1c9bc840cffcfdfdb58d04d0a646c05d7d0e40b7
ravilohana <ravi.lohana1987@gmail.com>
2018-10-11 00:25:29 +0530

    first java file

---
+++ TestProject1/src/TestClass.java
@@ -0,0 +1,11 @@
+
+public class TestClass {
+	
+	
+	static String learn = "Connect Ecplise to github";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(learn);
+	}
+
+}
